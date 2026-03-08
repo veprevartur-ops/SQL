f0f0b9e25fa7f97d5e4afa11eb555b4eb8f469f8
veprevartur-ops <veprev.artur@gmail.com>
2026-03-08 17:13:08 +0300

    Add files via upload

---
+++ Models/ExpenseItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+public class ExpenseItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int CategoryId { get; set; }
+    public ExpenseCategory? Category { get; set; }
+    public bool IsActive { get; set; }
+    [JsonIgnore]
+    public ICollection<ExpenseTransaction>? Transactions { get; set; }
+}
