2cec0d992d7c4c859baadfaec141c803f9589ecf
veprevartur-ops <veprev.artur@gmail.com>
2026-04-09 15:15:31 +0300

    Update computer_lessons.sql

--- computer_lessons.sql
+++ computer_lessons.sql
@@ -13,7 +13,7 @@ CREATE TABLE [Group] (
 );
 
 -- Студент
-CREATE TABLE Student (
+CREATE TABLE [Student] (
     StudentID UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
     FullName NVARCHAR(100) NOT NULL,
     BirthDate DATE,
@@ -24,7 +24,7 @@ CREATE TABLE Student (
 );
 
 -- Компьютерный класс
-CREATE TABLE Classroom (
+CREATE TABLE [Classroom] (
     ClassroomID UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
     RoomName NVARCHAR(50) NOT NULL,
     Capacity INT,
@@ -32,7 +32,7 @@ CREATE TABLE Classroom (
 );
 
 -- Компьютер
-CREATE TABLE Computer (
+CREATE TABLE [Computer] (
     ComputerID UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
     InventoryNumber NVARCHAR(50),
     Brand NVARCHAR(50),
@@ -44,7 +44,7 @@ CREATE TABLE Computer (
 );
 
 -- Занятие
-CREATE TABLE Lesson (
+CREATE TABLE [Lesson] (
     LessonID UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
     Topic NVARCHAR(200),
     LessonDate DATETIME NOT NULL,
@@ -54,7 +54,7 @@ CREATE TABLE Lesson (
 );
 
 -- Связь N:N Студент-Занятие
-CREATE TABLE StudentLesson (
+CREATE TABLE [StudentLesson] (
     StudentID UNIQUEIDENTIFIER NOT NULL,
     LessonID UNIQUEIDENTIFIER NOT NULL,
     Mark DECIMAL(4,2),
@@ -65,7 +65,7 @@ CREATE TABLE StudentLesson (
 );
 
 -- Связь N:N Компьютер-Занятие
-CREATE TABLE ComputerLesson (
+CREATE TABLE [ComputerLesson] (
     ComputerID UNIQUEIDENTIFIER NOT NULL,
     LessonID UNIQUEIDENTIFIER NOT NULL,
     IsUsed BIT,
@@ -78,40 +78,40 @@ CREATE TABLE ComputerLesson (
 
 --Вставка строк в таблицы
 INSERT INTO [dbo].[Group] (GroupID, GroupName, IsActive) VALUES
-(NEWID(), N'ПИ-101', 1),
-(NEWID(), N'ПИ-102', 1),
-(NEWID(), N'ПИ-103', 0);
+                          (NEWID(), N'ПИ-101', 1)
+                         ,(NEWID(), N'ПИ-102', 1)
+                         ,(NEWID(), N'ПИ-103', 0);
 
 INSERT INTO Student (StudentID, FullName, BirthDate, GPA, GroupID, IsActive) VALUES
-(NEWID(), N'Иванов Иван Иванович', '2003-05-12', 4.50, (SELECT TOP 1 GroupID FROM [Group] WHERE GroupName = N'ПИ-101'), 1),
-(NEWID(), N'Петров Петр Петрович', '2002-11-23', 3.80, (SELECT TOP 1 GroupID FROM [Group] WHERE GroupName = N'ПИ-102'), 1),
-(NEWID(), N'Сидорова Анна Сергеевна', '2004-01-30', 4.90, (SELECT TOP 1 GroupID FROM [Group] WHERE GroupName = N'ПИ-101'), 0);
+                    (NEWID(), N'Иванов Иван Иванович', '2003-05-12', 4.50, (SELECT TOP 1 GroupID FROM [Group] WHERE GroupName = N'ПИ-101'), 1)
+                   ,(NEWID(), N'Петров Петр Петрович', '2002-11-23', 3.80, (SELECT TOP 1 GroupID FROM [Group] WHERE GroupName = N'ПИ-102'), 1)
+                   ,(NEWID(), N'Сидорова Анна Сергеевна', '2004-01-30', 4.90, (SELECT TOP 1 GroupID FROM [Group] WHERE GroupName = N'ПИ-101'), 0);
 
 INSERT INTO Classroom (ClassroomID, RoomName, Capacity, IsActive) VALUES
-(NEWID(), N'101', 20, 1),
-(NEWID(), N'102', 25, 1),
-(NEWID(), N'103', 15, 0);
+                      (NEWID(), N'101', 20, 1)
+                     ,(NEWID(), N'102', 25, 1)
+                     ,(NEWID(), N'103', 15, 0);
 
 INSERT INTO Computer (ComputerID, InventoryNumber, Brand, PurchaseDate, Price, ClassroomID, IsActive) VALUES
-(NEWID(), N'INV-001', N'HP', '20220901', 45000.00, (SELECT TOP 1 ClassroomID FROM Classroom WHERE RoomName = N'101'), 1),
-(NEWID(), N'INV-002', N'Dell', '20210815', 42000.00, (SELECT TOP 1 ClassroomID FROM Classroom WHERE RoomName = N'102'), 1),
-(NEWID(), N'INV-003', N'Acer', '20200520', 39000.00, (SELECT TOP 1 ClassroomID FROM Classroom WHERE RoomName = N'101'), 0);
+                     (NEWID(), N'INV-001', N'HP', '20220901', 45000.00, (SELECT TOP 1 ClassroomID FROM Classroom WHERE RoomName = N'101'), 1)
+                    ,(NEWID(), N'INV-002', N'Dell', '20210815', 42000.00, (SELECT TOP 1 ClassroomID FROM Classroom WHERE RoomName = N'102'), 1)
+                    ,(NEWID(), N'INV-003', N'Acer', '20200520', 39000.00, (SELECT TOP 1 ClassroomID FROM Classroom WHERE RoomName = N'101'), 0);
 
 
 INSERT INTO Lesson (LessonID, Topic, LessonDate, ClassroomID, IsActive) VALUES
-(NEWID(), N'Основы программирования', '20240220 09:00:00', (SELECT TOP 1 ClassroomID FROM Classroom WHERE RoomName = N'101'), 1),
-(NEWID(), N'Базы данных', '20240221 11:00:00', (SELECT TOP 1 ClassroomID FROM Classroom WHERE RoomName = N'102'), 1),
-(NEWID(), N'Компьютерные сети', '20240222 13:00:00', (SELECT TOP 1 ClassroomID FROM Classroom WHERE RoomName = N'103'), 0);
+                   (NEWID(), N'Основы программирования', '20240220 09:00:00', (SELECT TOP 1 ClassroomID FROM Classroom WHERE RoomName = N'101'), 1)
+                  ,(NEWID(), N'Базы данных', '20240221 11:00:00', (SELECT TOP 1 ClassroomID FROM Classroom WHERE RoomName = N'102'), 1)
+                  ,(NEWID(), N'Компьютерные сети', '20240222 13:00:00', (SELECT TOP 1 ClassroomID FROM Classroom WHERE RoomName = N'103'), 0);
 
 INSERT INTO StudentLesson (StudentID, LessonID, Mark, IsPresent) VALUES
-((SELECT TOP 1 StudentID FROM Student WHERE FullName = N'Иванов Иван Иванович'), (SELECT TOP 1 LessonID FROM Lesson WHERE Topic = N'Основы программирования'), 5.00, 1),
-((SELECT TOP 1 StudentID FROM Student WHERE FullName = N'Петров Петр Петрович'), (SELECT TOP 1 LessonID FROM Lesson WHERE Topic = N'Базы данных'), 4.50, 1),
-((SELECT TOP 1 StudentID FROM Student WHERE FullName = N'Сидорова Анна Сергеевна'), (SELECT TOP 1 LessonID FROM Lesson WHERE Topic = N'Компьютерные сети'), 4.80, 0);
+                          ((SELECT TOP 1 StudentID FROM Student WHERE FullName = N'Иванов Иван Иванович'), (SELECT TOP 1 LessonID FROM Lesson WHERE Topic = N'Основы программирования'), 5.00, 1)
+                         ,((SELECT TOP 1 StudentID FROM Student WHERE FullName = N'Петров Петр Петрович'), (SELECT TOP 1 LessonID FROM Lesson WHERE Topic = N'Базы данных'), 4.50, 1)
+                         ,((SELECT TOP 1 StudentID FROM Student WHERE FullName = N'Сидорова Анна Сергеевна'), (SELECT TOP 1 LessonID FROM Lesson WHERE Topic = N'Компьютерные сети'), 4.80, 0);
 
 INSERT INTO ComputerLesson (ComputerID, LessonID, IsUsed) VALUES
-((SELECT TOP 1 ComputerID FROM Computer WHERE InventoryNumber = N'INV-001'), (SELECT TOP 1 LessonID FROM Lesson WHERE Topic = N'Основы программирования'), 1),
-((SELECT TOP 1 ComputerID FROM Computer WHERE InventoryNumber = N'INV-002'), (SELECT TOP 1 LessonID FROM Lesson WHERE Topic = N'Базы данных'), 1),
-((SELECT TOP 1 ComputerID FROM Computer WHERE InventoryNumber = N'INV-003'), (SELECT TOP 1 LessonID FROM Lesson WHERE Topic = N'Компьютерные сети'), 0);
+                           ((SELECT TOP 1 ComputerID FROM Computer WHERE InventoryNumber = N'INV-001'), (SELECT TOP 1 LessonID FROM Lesson WHERE Topic = N'Основы программирования'), 1)
+                          ,((SELECT TOP 1 ComputerID FROM Computer WHERE InventoryNumber = N'INV-002'), (SELECT TOP 1 LessonID FROM Lesson WHERE Topic = N'Базы данных'), 1)
+                          ,((SELECT TOP 1 ComputerID FROM Computer WHERE InventoryNumber = N'INV-003'), (SELECT TOP 1 LessonID FROM Lesson WHERE Topic = N'Компьютерные сети'), 0);
 
 
 
