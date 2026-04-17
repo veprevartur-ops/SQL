daae51cb13fdfafd8ea362f7b039d20b115a5966
veprevartur-ops <veprev.artur@gmail.com>
2026-04-17 13:23:45 +0300

    Delete Models directory

--- Models/ExpenseTransaction.cs
+++
@@ -1,9 +0,0 @@
-﻿public class ExpenseTransaction
-{
-    public int Id { get; set; }
-    public DateTime Date { get; set; }
-    public decimal Amount { get; set; }
-    public string Comment { get; set; }
-    public int ExpenseItemId { get; set; }
-    public ExpenseItem? ExpenseItem { get; set; }
-}
